ace291df7decb63502a5d8abb9ac766203dd1e63
nj <4769034+nianjie@users.noreply.github.com>
2021-11-13 09:14:31 +0900

    assign to the literal underbar as the symbol is reserved from fish.

--- underbar.fish
+++ underbar.fish
@@ -2,6 +2,6 @@ function underbar --description 'gulp every bit you put out into underbar'
   if not commandline | string length -q
     commandline -r $hisotry[1]
   end
-  fish_commandline_prepend 'set -g _ ('
+  fish_commandline_prepend 'set -g underbar ('
   fish_commandline_append ')'
 end
