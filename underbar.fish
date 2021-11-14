aed7ebf6176da83af307ad016ab71cd20909e100
nj <4769034+nianjie@users.noreply.github.com>
2021-11-14 17:50:14 +0900

    typo fix.

--- underbar.fish
+++ underbar.fish
@@ -1,6 +1,6 @@
 function underbar --description 'gulp every bit you put out into underbar'
   if not commandline | string length -q
-    commandline -r $hisotry[1]
+    commandline -r $history[1]
   end
   fish_commandline_prepend 'set -g underbar ('
   fish_commandline_append ')'
