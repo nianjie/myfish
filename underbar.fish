a118d7b5b111f33d77b63b88444c92197a80feb4
nj <4769034+nianjie@users.noreply.github.com>
2021-11-15 10:31:59 +0900

    remove underbar-affix from command line if exit.

--- underbar.fish
+++ underbar.fish
@@ -2,6 +2,14 @@ function underbar --description 'gulp every bit you put out into underbar'
   if not commandline | string length -q
     commandline -r $history[1]
   end
-  fish_commandline_prepend 'set -g underbar ('
-  fish_commandline_append ')'
+
+  set -l process (commandline)
+breakpoint
+  if string match -rq -- 'set -g underbar \((?<process>.*)\)' $process
+breakpoint
+    commandline -r -- $process
+  else 
+    fish_commandline_prepend 'set -g underbar ('
+    fish_commandline_append ')'
+  end
 end
