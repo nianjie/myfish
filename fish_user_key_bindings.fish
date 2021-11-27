1785af977e265b05a87896ef9e576df3861cbbb2
nj <4769034+nianjie@users.noreply.github.com>
2021-11-27 19:47:12 +0900

    change custom key binding.

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
-if not bind -s \cG &>/dev/null
-  bind \cG underbar
+if not bind -s \eg &>/dev/null
+  bind \eg underbar
+  bind \ei underbar_show
 else
-  set -l occupied (bind \cG | string split ' ' --)
+  set -l occupied (bind \eg | string split ' ' --)
   echo \aOops, $occupied[-1].
 end
